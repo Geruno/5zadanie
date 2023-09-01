81e8b2bf9eb76206862bcad6cf0a588e9c7d0d2d
Александр Д. Довгаль <sasha_d@puls-pro.local>
2023-09-01 15:11:36 +0700

    Вторая задача выполнена

--- 1zadanie/Program.cs
+++ 1zadanie/Program.cs
@@ -16,7 +16,7 @@ int Count(int[] array)
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] % 2 ==0) count++;
+        if (array[i] % 2 == 0) count++;
     }
     return count;
 }
--- 2zadanie/Program.cs
+++ 2zadanie/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите размер массива: ");
+string str_size = Console.ReadLine();
+int size = int.Parse(str_size);
+Console.WriteLine("Введите максимальное число в массиве: ");
+string str_max = Console.ReadLine();
+int max = int.Parse(str_max);
+int[] FillArray (int size, int max)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(max);
+    }
+    return array;
+}
+int Summ(int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0) summ += array[i];
+    }
+    return summ;
+}
+int[] array = FillArray(size, max);
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine($"Сумма чисел на нечетных позициях {Summ(array)}");
