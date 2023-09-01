53af8c8fcac654ef599e84150360a055e2299a2e
Александр Д. Довгаль <sasha_d@puls-pro.local>
2023-09-01 15:50:44 +0700

    Третья задача выполнена

--- 3zadanie/Program.cs
+++ 3zadanie/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите размер массива: ");
+string str_size = Console.ReadLine();
+int size = int.Parse(str_size);
+Console.WriteLine("До какого числа генерация: ");
+string str_max = Console.ReadLine();
+int max = int.Parse(str_max);
+Console.WriteLine("Сколько цифр после запятой: ");
+string str_count = Console.ReadLine();
+int count = int.Parse(str_count);
+double[] FillArray (int size, int max, int count)
+{
+    double[] array = new double[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Math.Round(rand.NextDouble() * max, count);
+    }
+    return array;
+}
+double Min(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+double Max(double[] array)
+{
+    double max = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+double Razn(double[] array)
+{
+    double razn = Max(array) - Min(array);
+    return razn;
+}
+double[] array = FillArray(size, max, count);
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine($"Разница между максимальным и минимальным {Razn(array)}");
