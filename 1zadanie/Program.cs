01540e4e934b784f18bcfd8df6af14ce738cabdd
Александр Д. Довгаль <sasha_d@puls-pro.local>
2023-09-01 15:01:29 +0700

    Первая задача выполнена

--- 1zadanie/Program.cs
+++ 1zadanie/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите размер массива: ");
+string str_size = Console.ReadLine();
+int size = int.Parse(str_size);
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(100,1000);
+    }
+    return array;
+}
+int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 ==0) count++;
+    }
+    return count;
+}
+int[] array = FillArray(size);
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine($"Количество четных чисел {Count(array)}");
